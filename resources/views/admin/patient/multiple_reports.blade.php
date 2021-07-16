7b3992a39c9117fb8579a3ab7894d31052b7e378
farhankhan8 <mfarhanahmed417@gmail.com>
2021-07-16 21:42:21 +0500

    again

---
+++ resources/views/admin/patient/multiple_reports.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.ckediter')
+@section('content')
+
+ 
+
+    @php $previous_category_id="";
+    $i = 4;
+    
+    @endphp
+  
+    @foreach($tests as $testPerformedsId)
+    @php $i = $testPerformedsId->resultValueCount + $i;  echo $i;   @endphp
+        <div style="{{($testPerformedsId->category_id!=$previous_category_id && $previous_category_id!='' || $i > 9) ? 'page-break-before: always':''}}" class="card">
+
+        <!-- <div style="page-break-after: always;" class="card {{($testPerformedsId->category_id!=$previous_category_id && $previous_category_id!="") ? "page-break-before":""}}"> -->
+            @if($testPerformedsId->category_id!=$previous_category_id)
+                @include("admin.TestPerformed.partial_patient")
+            @endif
+            @include("admin.TestPerformed.partial_report")
+        </div>
+        @php
+        if($i>10){
+            $i= $testPerformedsId->resultValueCount;
+            }
+            $previous_category_id=$testPerformedsId->category_id;
+        @endphp
+    @endforeach
+
+    <div class="col-md-12 mb-12 noprint">
+        <button class="btn btn-primary" onclick="window.print()">Print Report</button>
+    </div>
+
+
+@endsection
